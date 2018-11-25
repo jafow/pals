1c0d6c9740949efa6cc732d3498dc12af7a65d88
jafow <jared.a.fowler@gmail.com>
2018-11-25 11:15:49 -0800

    4

--- examples/detect_single_from_file.rs
+++ examples/detect_single_from_file.rs
@@ -12,8 +12,29 @@ fn main() {
     f.read_to_string(&mut lines)
         .expect("error reading contents of file");
 
+    // store all scores from each line
+    let mut scores: Vec<pals::LineScore> = Vec::new();
     for l in lines.lines() {
+        let line_string = l.to_string();
+        let score = pals::LineScore {
+            fscore: pals::xor::single_byte(l),
+            line: line_string
+        };
+        scores.push(score);
+    }
+    let mut min_score = pals::LineScore {
+        line: String::from("Nada"),
+        fscore: pals::FreqScore {id: 0, score: 10.0}
+    };
+
+    for score in scores.iter() {
+        if score.fscore.score <= min_score.fscore.score {
+            min_score.fscore.score = score.fscore.score;
+            min_score.fscore.id = score.fscore.id;
+            min_score.line = score.line;
+        }
     }
 
+    println!("Min score: {:?} for char: {:?} on line {:?}", min_score.fscore.score, min_score.fscore.id, min_score.line);
     assert_eq!(0, 0);
 }
