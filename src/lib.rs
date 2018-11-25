6f779867e83d5bb4ff6534ac63c8aaa8e8dbc608
jafow <jared.a.fowler@gmail.com>
2018-11-25 11:15:38 -0800

    line score type for multilines

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 pub mod xor;
 pub use xor::{xor_fixed, single_byte};
 
+#[derive(Clone)]
 pub struct FreqScore {
     pub id: u8,
     pub score: f32
 }
+
+#[derive(Clone)]
+pub struct LineScore {
+    pub fscore: FreqScore,
+    pub line: String
+}
+
 pub mod table;
 pub use table::{char_freq};
